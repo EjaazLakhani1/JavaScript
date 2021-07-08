931b2b9134958227dfc7734a6ca3e07f00d6fcb4
EjaazLakhani1 <ejaaz.lakhani1@ucalgary.ca>
2021-07-07 23:25:49 -0600

    code for post lecture assignments

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,79 @@
+/*
 let js = 'amazing'
 console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstanme = "Jonas";
+
+console.log(firstanme);
+console.log(firstanme);
+console.log(firstanme);
+
+let jonas_matilda = "JM";
+
+let PI = 3.1415
+
+let myFirstJob = "Intern"
+let myCurrentJob = "Intern"
+
+
+let javascriptIsFun = true;
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = 'yes'
+console.log(typeof javascriptIsFun)
+
+let year
+console.log(typeof year)
+
+year = 1998
+console.log(typeof year)
+
+console.log(typeof null)
+
+
+let age = 23;
+age = 24;
+
+const birthYear = 1998;
+//birthYear = 2000; 
+*/
+
+const ageJonas = 2037 - 1998;
+const ageSarah = 2
+
+/*
+Below is code for assignments for this section
+*/
+
+//Lecture 10: Values and Variables
+const country = 'Canada';
+const continent = 'North America';
+let population = 5000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//Lecture 12: Data Types
+const isIsland = false;
+let language
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
+
+//Lecture 13: let, const and var
+language = "english"
+
+//Lecture 14: Basic Operators
+const halfPopulation = population/2;
+console.log(population++);
+console.log(population > 6000000);
+const average = 33000000
+console.log(population > average);
+const description = 'Portugal is in Europe, and its 11 million people speak portuguese'
+
