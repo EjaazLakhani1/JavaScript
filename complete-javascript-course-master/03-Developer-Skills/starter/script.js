9cbf7ea7e90973fa4766bc8a21227ff9f385ff2e
EjaazLakhani1 <ejaaz.lakhani1@ucalgary.ca>
2021-08-10 00:02:39 -0600

    lesson 3 and first project

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,3 +1,24 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const x = 23;
+
+const func = x => {
+    return x;
+};
+
+console.log("hi");
+
+// Coding Challenge # 1
+
+const data1 = [17, 21, 23];
+
+const printForecast = temps => {
+    let output = ``;
+    for (let i = 0; i < temps.length; i++) {
+         output += `${temps[i]}\u00B0 C in ${i} days ... `;
+    }
+    return output;
+}
+
+console.log(printForecast(data1));
--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,53 @@
 'use strict';
+
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+*/
+
+let targetNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = message => {
+    document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    targetNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.score').textContent = score;
+    displayMessage('Start guessing...');
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+});
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+
+    if (!guess) {
+        displayMessage('Please enter a number');
+    } else if (guess !== targetNumber) {
+        if (score >= 0) {
+            displayMessage(guess < targetNumber ? 'Too Low' : 'Too High');
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            displayMessage('You lose');
+        }
+    } else if (guess === targetNumber) {
+        displayMessage('BINGO');
+        document.querySelector('.number').textContent = targetNumber;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+        if (score > highScore) {
+            document.querySelector('.highscore').textContent = score;
+        }
+    }
+});
